a32084cc7179240658c68d3cbe1c2ba291360809
lyj7111 <lyj7111@gmail.com>
2024-09-25 10:53:04 +0900

    feat: post model, serializers

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="post",
+    )
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    image = models.ImageField(upload_to="posts/image/", null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_post", blank=True)
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListAPIView):
+    pass
