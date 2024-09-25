5d8e51670ea0829f59117c43fb19ebc4e193d231
nahyeons <skgus3721@gmail.com>
2024-09-25 20:38:18 +0900

    프로필조회 및 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from .models import User
 from rest_framework import serializers
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -7,3 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "username",
         )
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    # username = serializers.CharField(source='user.username', read_only=True)  # User 모델의 username, email 필드 가져오기
+    # email = serializers.EmailField(source='user.email', read_only=True) 
+
+    class Meta:
+        model = User
+        fields = ['profile_image', "username", "email"]  # 반환 및 수정할 필드 목록
