867fa1f83aaf1ea7705ffd83dad8d4283538e597
lyj7111 <lyj7111@gmail.com>
2024-09-26 18:55:39 +0900

    feat: 좋아요 기능 구현(post 모델, 시리얼라이저 일부 수정했습니다.)

--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post"),
-    path("<int:pk>/", views.PostDetailView.as_view()),
+    path("<int:post_pk>/", views.PostDetailView.as_view()),
     path("<int:post_pk>/comment/", views.CommentLIstCreateView.as_view()),
     path(
         "comment/<int:pk>",
         views.CommentUpdateDeleteView.as_view(),
     ),
+    path("<int:post_pk>/like/", views.PostLikeView.as_view()),
 ]
