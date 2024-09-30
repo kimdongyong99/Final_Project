56bc2ca83fe401377c7aa9a399517ec05b9dee66
Kyuho09 <noaetoile1@gmail.com>
2024-09-30 17:51:08 +0900

    feat: crawler 수정
    cralwer 앱 수정
    migrations 정리

--- crawler/urls.py
+++ crawler/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CrawlerNewsList, ArticleSummarizer, CrawlerHealthChosun
+from .views import CrawlerNewsList, CrawlerHealthChosun
+# from .views import ArticleSummarizer
 
 urlpatterns = [
     path('news/', CrawlerNewsList.as_view(), name='news_list'),
     path('Chosun/', CrawlerHealthChosun.as_view(), name='news_list'),
-    path('summarize/', ArticleSummarizer.as_view(), name='summarize'),
+    # path('summarize/', ArticleSummarizer.as_view(), name='summarize'),
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -10,40 +10,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
-from django.http import HttpResponse
-from django.views import View
-from bs4 import BeautifulSoup
-import requests
-
-from django.http import HttpResponse
-from django.views import View
-from bs4 import BeautifulSoup
-import requests
-
-
-from django.http import HttpResponse
-from django.views import View
-from bs4 import BeautifulSoup
-import requests
-
-
-from django.http import HttpResponse
-from django.views import View
-from bs4 import BeautifulSoup
-import requests
-
 class CrawlerHealthChosun(View):
     def get(self, request, *args, **kwargs):
         # 헬스 조선 기사 리스트 페이지 URL
         url = "https://health.chosun.com/list_life.html"
         response = requests.get(url, verify=True)
 
-        # 응답의 인코딩을 euc-kr로 설정
+        # 응답의 인코딩을 euc-kr로 설정, 인코딩 안하면 깨짐
         response.encoding = 'euc-kr'
 
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # 기사 제목과 링크를 추출
+        # 기사 제목, 링크를 추출
         article_info = self.extract_articles(soup)
 
         return HttpResponse(f"크롤링 시작:<br>{article_info}")
@@ -51,7 +29,7 @@ class CrawlerHealthChosun(View):
     def extract_articles(self, soup):
         article_info = ""
         
-        # 헬스 조선 기사 목록에서 제목과 링크 추출
+        # 제목과 링크 추출
         article_list = soup.find_all('li', class_='rellist')
 
         for item in article_list:
@@ -63,9 +41,6 @@ class CrawlerHealthChosun(View):
                 article_info += f'<a href="{full_link}">{title}</a><br>'
         
         return article_info
-
-
-
 
 
 class CrawlerNewsList(View):
@@ -92,60 +67,60 @@ class CrawlerNewsList(View):
         return news_info
 
 
-class WebDriverManager:
-    def __enter__(self):
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+# class WebDriverManager:
+#     def __enter__(self):
+#         chrome_options = Options()
+#         chrome_options.add_argument("--headless")
+#         chrome_options.add_argument("--no-sandbox")
+#         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        self.driver = webdriver.Chrome(
-            service=ChromeService(executable_path="/opt/homebrew/bin/chromedriver"),
-            options=chrome_options,
-        )
-        return self.driver
+#         self.driver = webdriver.Chrome(
+#             service=ChromeService(executable_path="/opt/homebrew/bin/chromedriver"),
+#             options=chrome_options,
+#         )
+#         return self.driver
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.driver.quit()
+#     def __exit__(self, exc_type, exc_value, traceback):
+#         self.driver.quit()
 
 
-class ArticleSummarizer(View):
-    def get(self, request, *args, **kwargs):
-        url = request.GET.get("url")
+# class ArticleSummarizer(View):
+#     def get(self, request, *args, **kwargs):
+#         url = request.GET.get("url")
 
-        if not url:
-            return JsonResponse({"error": "URL이 제공되지 않았습니다."}, status=400)
+#         if not url:
+#             return JsonResponse({"error": "URL이 제공되지 않았습니다."}, status=400)
 
-        try:
-            with WebDriverManager() as driver:
-                driver.get(url)
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located(
-                        (By.CLASS_NAME, "NewsEndMain_comp_article_content__PZYoE")
-                    )
-                )
-                html_content = driver.page_source
-        except Exception as e:
-            return JsonResponse(
-                {"error": f"웹 크롤링 중 오류 발생: {str(e)}"}, status=500
-            )
+#         try:
+#             with WebDriverManager() as driver:
+#                 driver.get(url)
+#                 WebDriverWait(driver, 10).until(
+#                     EC.presence_of_element_located(
+#                         (By.CLASS_NAME, "NewsEndMain_comp_article_content__PZYoE")
+#                     )
+#                 )
+#                 html_content = driver.page_source
+#         except Exception as e:
+#             return JsonResponse(
+#                 {"error": f"웹 크롤링 중 오류 발생: {str(e)}"}, status=500
+#             )
 
-        soup = BeautifulSoup(html_content, "html.parser")
-        content = self.extract_main_content(soup)
+#         soup = BeautifulSoup(html_content, "html.parser")
+#         content = self.extract_main_content(soup)
 
-        if not content:
-            return JsonResponse({"error": "기사를 찾을 수 없습니다."}, status=404)
+#         if not content:
+#             return JsonResponse({"error": "기사를 찾을 수 없습니다."}, status=404)
 
-        return JsonResponse({"article_content": content})
+#         return JsonResponse({"article_content": content})
 
-    def extract_main_content(self, soup):
-        article_div = soup.find(
-            "article", class_="NewsEndMain_comp_news_article__wMpnW _article_body"
-        )
-        if article_div:
-            content_div = article_div.find("div", class_="_article_content")
-            if content_div:
-                text = content_div.get_text(strip=True)
-                if len(text) > 200:
-                    return text
-        return None
+#     def extract_main_content(self, soup):
+#         article_div = soup.find(
+#             "article", class_="NewsEndMain_comp_news_article__wMpnW _article_body"
+#         )
+#         if article_div:
+#             content_div = article_div.find("div", class_="_article_content")
+#             if content_div:
+#                 text = content_div.get_text(strip=True)
+#                 if len(text) > 200:
+#                     return text
+#         return None
