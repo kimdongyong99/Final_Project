e8df4e9750c52838b64e6aa6084f23ea62407b9a
Kyuho09 <noaetoile1@gmail.com>
2024-10-01 17:04:41 +0900

    crawling fix

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=300)
+    link = models.URLField(max_length=500)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- articles/urls.py
+++ articles/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import CrawlerNewsList, CrawlerHealthChosun
 
-urlpatterns = []
+urlpatterns = [
+    path('news/', CrawlerNewsList.as_view(), name='news_list'),
+    path('Chosun/', CrawlerHealthChosun.as_view(), name='news_list'),
+]
