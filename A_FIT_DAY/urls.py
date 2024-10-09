d0ccfd72bd089a9425afe39aad5ab51f1ecf7fe4
Kyuho09 <noaetoile1@gmail.com>
2024-10-09 13:30:39 +0900

    feat: 프론트 구현
    회원가입, 로그인 프론트 구현
    주소 필드 추가

--- A_FIT_DAY/urls.py
+++ A_FIT_DAY/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -28,8 +29,12 @@ urlpatterns = [
     path("api/articles/", include("articles.urls")),
     path("api/chat_gpt/", include("chatgpt.urls")),
     path("api/payment/", include("payment.urls")),
+
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
+
 
 # 미디어 파일 제공 설정 추가
 if settings.DEBUG:  # DEBUG 모드일 때만 미디어 파일을 제공
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
