7a8263c50b72edf64c78ebe2d12607ed4426eab1
lyj7111 <lyj7111@gmail.com>
2024-10-11 12:29:20 +0900

    받지 마시오.(pull 받으려고 하는중)

--- articles/urls.py
+++ articles/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from .views import CrawlerNewsList, ArticleSummarizer, ArticleDetailAPIView, ArticleLikeView
 
-urlpatterns = []
+urlpatterns = [
+    path('news/', CrawlerNewsList.as_view()),
+    path('summarize/', ArticleSummarizer.as_view()),
+    path('<int:pk>/', ArticleDetailAPIView.as_view()),
+    path('<int:article_id>/like/', ArticleLikeView.as_view()),
+]
