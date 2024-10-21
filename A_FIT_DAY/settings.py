bf1b025b134b99e2376fa2bca250587ee460cabe
kimdongyong99 <homeland007@naver.com>
2024-10-21 20:09:59 +0900

    주소추가

--- A_FIT_DAY/settings.py
+++ A_FIT_DAY/settings.py
@@ -88,7 +88,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",  # 3000 포트 추가
-    "http://localhost:3000",  # 3000 포트 추가
+    "http://localhost:3000",
+    "http://13.125.200.162",
 ]
 # CORS_ALLOW_CREDENTIALS = True
 
